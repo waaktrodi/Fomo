2f743e4002b5a1219bac8a0250d53f84c8f5b39e
waaktrodi <ikhsanakbar2008@gmail.com>
2026-02-03 08:21:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,269 @@
+const students = [
+  {
+    "id": "0fb2a5e8-f2a4-4885-b80f-ceee5ed5d7f6",
+    "nis": "13508",
+    "name": "Anandhita Syakira Hidayat",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:03:26"
+  },
+  {
+    "id": "e5365705-9bfa-4f54-8827-f3a671bd9347",
+    "nis": "13518",
+    "name": "Anisa Mulia",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:06:24"
+  },
+  {
+    "id": "fd9451af-852b-42b3-b34e-f64823d54418",
+    "nis": "13523",
+    "name": "Aqbiel Juand Dwirika Rizki Ramadhan",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:06:58"
+  },
+  {
+    "id": "a14d7ec3-2e5a-4004-840c-3505d77acfd7",
+    "nis": "13528",
+    "name": "Aries Indra Prakoso",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:07:31"
+  },
+  {
+    "id": "f47fa413-7fce-4da1-b8d3-e4e93ed79de8",
+    "nis": "13561",
+    "name": "Bernessa Rahadatulaisy virbara",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:08:20"
+  },
+  {
+    "id": "3ee7c165-ac4c-4b7f-80ce-2421d2c52adc",
+    "nis": "13596",
+    "name": "Dhea Anandasyafira",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:09:58"
+  },
+  {
+    "id": "d49b877c-d9c7-466f-8aa3-3c09135ff7c0",
+    "nis": "13597",
+    "name": "Dhea Lucita Sabrina Sakhi",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:10:31"
+  },
+  {
+    "id": "00d5c868-5ec3-4a15-98c4-c78a8037b0e5",
+    "nis": "13627",
+    "name": "Fandy Fitrianto",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:13:23"
+  },
+  {
+    "id": "3525d5a0-e379-408c-aba9-314f7a5baedc",
+    "nis": "13637",
+    "name": "Farius Cinta Alifiu Fahamsa",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:14:00"
+  },
+  {
+    "id": "d970529b-b52f-4bea-ae7a-688da41187dc",
+    "nis": "13640",
+    "name": "Fatih Muttaqin Azka",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:15:01"
+  },
+  {
+    "id": "c0908f19-143a-45c4-ae78-47582af6b703",
+    "nis": "13642",
+    "name": "Febrian Immanuel Hati Pratama",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:15:56"
+  },
+  {
+    "id": "1fd53355-c126-416e-aea0-1318d7499e00",
+    "nis": "13652",
+    "name": "Gerald Arvin Aryasetha",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:16:35"
+  },
+  {
+    "id": "f0079403-635d-480d-bc9f-945c2fc23169",
+    "nis": "13658",
+    "name": "Grevaldio Putra Alexa Evano",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:16:59"
+  },
+  {
+    "id": "dad76ca8-4592-45e6-bc60-baf784f4b740",
+    "nis": "13659",
+    "name": "Hafizh Khaizuran Fauzi",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:17:37"
+  },
+  {
+    "id": "40123f1b-9b78-4c82-8fcc-abe390c5f094",
+    "nis": "13660",
+    "name": "Hana Atikah Jauza",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:18:03"
+  },
+  {
+    "id": "d421862e-1c38-4995-b242-2ab6c0f347d4",
+    "nis": "13667",
+    "name": "Inggrid Nikita Prasetya",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:18:35"
+  },
+  {
+    "id": "40147e0f-bf65-46aa-b341-cbd42b8a927b",
+    "nis": "13680",
+    "name": "Jethro Ata Alvarean",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:19:01"
+  },
+  {
+    "id": "460f948a-852b-46a2-95bc-6a781fe777a3",
+    "nis": "13683",
+    "name": "Joy Abimanyu Hapyputra",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:19:28"
+  },
+  {
+    "id": "9fe41fa1-ce84-4047-8ed2-745661e4f8a2",
+    "nis": "13684",
+    "name": "Kafka Xavier Hazza Pramono",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:20:06"
+  },
+  {
+    "id": "d30b6136-8713-4df9-8584-6ad6ffb6893f",
+    "nis": "13685",
+    "name": "Kanaya Karin Rubiawan",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:20:29"
+  },
+  {
+    "id": "4312a7af-b703-4f62-a452-75e5168ef816",
+    "nis": "12686",
+    "name": "Karin Vanessa Krisdiva",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:21:00"
+  },
+  {
+    "id": "55f72f9f-11b4-49a5-a0e2-513360254e8f",
+    "nis": "13693",
+    "name": "Kenzi Hafiz Susanto",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:21:38"
+  },
+  {
+    "id": "0120201f-8d61-4bd3-9e25-3e870b915422",
+    "nis": "13748",
+    "name": "Mohammad Fadhil Wafi",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:24:13"
+  },
+  {
+    "id": "739c00c9-e660-4b51-a459-d91ad2ed8513",
+    "nis": "13754",
+    "name": "Mohammad Ikhsan Al Akbar",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:24:32"
+  },
+  {
+    "id": "35e88291-0a3b-4d93-bd27-059e65deb833",
+    "nis": "13739",
+    "name": "Monica Dewi Santoso",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:22:09"
+  },
+  {
+    "id": "385868f5-418d-4412-81c6-24244e22ab26",
+    "nis": "13742",
+    "name": "Muhammad Akhtar Razan",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:23:11"
+  },
+  {
+    "id": "009aaa1b-76a9-4ca9-88c6-efa0cc9dad6d",
+    "nis": "13745",
+    "name": "Muhammad Brahmana Adu",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:23:33"
+  },
+  {
+    "id": "6cc9d470-a3af-42f1-aaff-30676a18ff2b",
+    "nis": "13718",
+    "name": "Muhammad Fadhil Toriq",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:22:42"
+  },
+  {
+    "id": "80429298-6ec5-473d-87de-4ce41f56cf98",
+    "nis": "13758",
+    "name": "Muhammad Riko Setya Wiradinata",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:25:08"
+  },
+  {
+    "id": "68c2b0bc-a7fd-4aa4-bba3-544235fb6f64",
+    "nis": "13763",
+    "name": "Mutiara Sanniyah",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:27:40"
+  },
+  {
+    "id": "79235c06-79a0-4201-81ef-8d65da9d67ab",
+    "nis": "13768",
+    "name": "Nadhief Fathan Arrafif",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:25:51"
+  },
+  {
+    "id": "cea856d1-1cb7-456b-8096-a872ad607c45",
+    "nis": "13778",
+    "name": "Nafisah Dzatir Rajwa",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:26:23"
+  },
+  {
+    "id": "e92164bb-9ed4-47dd-8416-b02e404fd7b0",
+    "nis": "13824",
+    "name": "Rafasyah Madang Arkadipa",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:26:55"
+  },
+  {
+    "id": "9e857189-0f52-458d-96e4-361473dc5e44",
+    "nis": "13841",
+    "name": "Reza Emmeraldi",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:27:59"
+  },
+  {
+    "id": "f288b45f-07e9-42e5-8cd6-a525c71a37f3",
+    "nis": "13885",
+    "name": "Velisa Khalilah",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:28:28"
+  },
+  {
+    "id": "354b7aa5-6680-412a-bfd1-dd6dc8dc1917",
+    "nis": "13895",
+    "name": "Zaki Rahmat Fahrezi",
+    "Class_id": "XI-C1",
+    "Created_at": "2026-01-27 08:28:48"
+  }
+];
+
+const tbody = document.getElementById("data-siswa");
+
+students.forEach((student, index) => {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${student.nis}</td>
+    <td>${student.name}</td>
+    <td>${student.class_id}</td>
+  `;
+
+  tbody.appendChild(row);
+});
